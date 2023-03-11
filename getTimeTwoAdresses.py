b4cfd95f6ec5e7b1837a66f5648bc6751e0927b0
keksShmeks1 <60845636+keksShmeks1@users.noreply.github.com>
2023-03-11 17:51:30 +0300

    Update getTimeTwoAdresses.py

--- getTimeTwoAdresses.py
+++ getTimeTwoAdresses.py
@@ -2,17 +2,8 @@
 import requests
 from adressToCoords import getCoords
 
-# Задаем адреса начальной и конечной точек
-origin = 'Саратов, проспект строителей 1'
-destination = 'Саратов улица Лунная 41а'
-
-# originCoords = getCoords(origin)
-# destinationCoords = getCoords(destination)
-# print(originCoords)
-# print(destinationCoords)
-
 # Задаем ключ API
-api_key = '3FMbqq8CXlmYwxzrALFPFZn4CGnRVIGifPzflGAhY6E'
+api_key = ''
 
 def getTimeHours(origin, destination):
     originCoords = getCoords(origin)
@@ -36,19 +27,3 @@ def getDistanceKilometers(origin, destination):
     return round(distance, 1)
 
 
-
-
-
-
-# Формируем URL для запроса маршрута
-
-# url = f'https://router.hereapi.com/v8/routes?origin={originCoords[0]},{originCoords[1]}&destination={destinationCoords[0]},{destinationCoords[1]}&return=summary,typicalDuration&transportMode=car&apikey={api_key}'
-#
-# # Отправляем запрос и получаем ответ
-# response = requests.get(url)
-# route = response.json()
-#
-# # Получаем информацию о маршруте
-# travel_time = route["routes"][0]["sections"][0]["summary"]["typicalDuration"]
-# # Выводим результат
-# print(f"Время в пути от {origin} до {destination} на основе типичного времени дня: {round(travel_time/60/60, 1)} Часов")
